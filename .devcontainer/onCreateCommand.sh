cc1b6843e34f015b60956d7ca77ca9df1bfce210
hareya0622 <nakada.developer.0622@gmail.com>
2023-11-23 17:08:06 +0000

    chore: update container build configuration

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 以下のセキュリティ対策への対策
+# https://github.blog/2022-04-12-git-security-vulnerability-announced/ 
+git config --global --add safe.directory /workspace
+
+# tab補完が動作するようにinstall
+apt update
+apt install -y bash-completion
+
+# gitコマンド補完用スクリプトのダウンロード
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O /etc/bash_completion.d/git
+
+echo 'source /etc/bash_completion' >> ~/.bashrc
