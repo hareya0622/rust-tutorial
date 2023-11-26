1a8fdd66d5d68c6549f5353524b7636a3dd1b812
hareya0622 <nakada.developer.0622@gmail.com>
2023-11-26 06:26:05 +0000

    add branch name to ps1

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -12,3 +12,10 @@ apt install -y bash-completion
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O /etc/bash_completion.d/git
 
 echo 'source /etc/bash_completion' >> ~/.bashrc
+
+# PS1にbranch名を追加
+echo 'parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1)/"
+}
+
+export PS1="\u@\h \w\$(parse_git_branch) \$ "' >> ~/.bashrc
